169bccfb2dfad71f847f9c64a0eb05ea1fea2cf3
Alberto Samele <alberto@nodalview.com>
2022-09-05 00:11:08 +0200

    Animating buttons on tap

--- Game of Life/Game of Life/showHomeScreen/HomeScreenView.swift
+++ Game of Life/Game of Life/showHomeScreen/HomeScreenView.swift
@@ -37,6 +37,12 @@ private extension HomeScreenView {
     let title: String
     let onTap: () -> Void
     var color: Color = AppAppearance.Colors.color_313031
+    var animationDuration: Double = 0.1
+    var animationOffset: CGFloat = 3
+    @State
+    private var yOffset: CGFloat = 0
+    @State
+    private var isAnimating: Bool = false
     
     // MARK: - Body
     
@@ -49,12 +55,31 @@ private extension HomeScreenView {
       .buttonStyle(StaticButtonStyle())
       .frame(maxWidth: .infinity, maxHeight: .infinity)
       .border(color, width: 2)
+      .offset(x: 0, y: yOffset)
     }
     
     // MARK: - Private methods
     
     private func onButtonTapped() {
+      guard !isAnimating else { return }
+      isAnimating = true
       
+      withAnimation(
+        .easeInOut(duration: animationDuration / 2)
+      ) {
+        yOffset = animationOffset
+      }
+      withAnimation(
+        .easeInOut(duration: animationDuration / 2)
+        .delay(animationDuration / 2)
+      ) {
+        yOffset = 0
+      }
+      
+      DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
+        isAnimating = false
+        onTap()
+      }
     }
   }
 }
