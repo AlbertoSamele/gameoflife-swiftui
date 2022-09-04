ce18e1cfe087cc6ece187d8e536966848d076ac2
Alberto Samele <alberto@nodalview.com>
2022-09-04 22:51:08 +0200

    Added black and white colors

---
+++ Game of Life/Game of Life/appearance/AppAppearance.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+struct AppAppearance {
+  struct Colors {
+    /// White equivalent
+    static let color_ffffff = Color("color_ffffff")
+    /// Black equivalent
+    static let color_313031 = Color("color_313031")
+  }
+}
